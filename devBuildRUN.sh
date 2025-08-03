5a312c2ded4c15b4ee4502961912f75449743cdb
jalaj <jalajghuge.official@gmail.com>
2025-08-03 11:39:31 +0000

    init

--- devBuildRUN.sh
+++
@@ -1,3 +0,0 @@
-docker container rm -f portfolio-frontend &&
-docker run -d --name portfolio-frontend -v /home/jaggyjalaj/Documents/portfolio/build/static:/usr/share/nginx/html -p 8080:80 -e CHOKIDAR_USEPOLLING=true portfolio-frontend &&
-yarn watch
--- prodBuildRUN.sh
+++
@@ -1,3 +0,0 @@
-docker service rm portfolio-frontend &&
-docker build -t portfolio-frontend-image . &&
-docker service create --name portfolio-frontend --network myOverlayNetwork portfolio-frontend-image 
